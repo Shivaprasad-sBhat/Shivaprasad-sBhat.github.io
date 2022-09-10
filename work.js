acdb9fc477d63d1943ead6cea5f59a1ef03af0d2
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-09-11 00:16:03 +0530

    home

---
+++ work.js
@@ -0,0 +1,20 @@
+
+let skill=["Java","SpringBoot","Hibernate","MySQL","AWS","JavaScript","HTML","CSS"];
+
+
+function skillDisplay(){
+    let n=0;
+    for(let i=0;i<skill.length;i++){
+        console.log(skill[i],n++);
+
+      
+      }
+     
+}
+
+skillDisplay();
+   
+  
+
+
+
