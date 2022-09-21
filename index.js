ca508b030e97fc6ea47af8a99e770c65a468634b
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-09-21 20:13:10 +0530

    responsive page

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function openmenu(){
 function closemenu(){
     sidemeu.style.right = "-200px";
 }
+
+
+
+  
+    // typing text animation script
+    var typed = new Typed(".typing", {
+      strings: ["Java Backend Developer"],
+      typeSpeed: 100,
+      backSpeed: 120,
+      loop: true,
+    });
+  
+    var typed = new Typed(".typing-2", {
+      strings: ["Java Backend Developer"],
+      typeSpeed: 100,
+      backSpeed: 120,
+      loop: true,
+    });
+  
+    
